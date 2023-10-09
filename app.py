ae5f663cad911cbb86316989d2adf614bdf7190d
Luis Munoz <134253222+Luis-Izaguirre@users.noreply.github.com>
2023-10-08 22:34:52 -0700

    Add Version2 files via upload
    New files list are in the read me.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL, MySQLdb
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import MySQLdb.cursors, re, hashlib
 import bcrypt 
 
 app = Flask(__name__)
@@ -8,31 +10,135 @@ app.secret_key = "thisissecret"
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] ='root'
-app.config['MYSQL_PASSWORD'] = 'Lambda-3000'
+app.config['MYSQL_PASSWORD'] = '???????'
 app.config['MYSQL_DB'] = 'com440'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
 
+# http://localhost:5000/pythonlogin/
+@app.route('/pythonlogin/', methods=['GET', 'POST'])
+def login():
+    # Output message if something goes wrong...
+    msg = ''
 
-@app.route('/')
-def home():
-    return render_template("home.html")
+    # check if "username" and "password" POST request exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
 
-@app.route('/signup', methods=['GET','POST'])
+        
+        # retrieve the hashed password
+        hash = password + app.secret_key
+        hash = hashlib.sha1(hash.encode())
+        password = hash.hexdigest()
+        
+
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM user WHERE username = %s AND password = %s', (username, password,))
+
+        # Fetch one record and return the result
+        account = cursor.fetchone()
+
+        #If account exists in accounts table in our db
+        if account:
+            # Create session data, we can access this data in other routes (Sorta like server cookies, will refrence later)
+            session['loggedin'] = True
+            session['username'] = account['username']
+            #redirect to home page
+            return redirect(url_for('home'))
+        else:
+            # Account doesn't exist or username/password incorrect
+            msg= 'Incorrect username/password!'
+
+    return render_template('index.html', msg=msg)
+
+@app.route('/pythonlogin/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
+
+
+# http://localhost:5000/signup - this will be the registration page
+@app.route('/pythonlogin/register', methods=['GET','POST'])
 def register():
-    if request.method == 'GET':
-        return render_template("signup.html")
-    else:
-       username = request.form['uname']
-       firstname = request.form['fname']
 
-       lastname = request.form['lname']
+    # Output message if something goes wrong..
+    msg=''
+
+    #Check if "username".. Post request exists (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'firstname' in request.form and 'lastname' in request.form and 'email' in request.form and 'password' in request.form:
+        
+        # Create variables for easy access
+       username = request.form['username']
+       firstname = request.form['firstname']
+       lastname = request.form['lastname']
        email = request.form['email']
        password = request.form['password']
 
+       # Check if account exists using MySQL
+       cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+       cursor.execute('SELECT * FROM user WHERE username = %s', (username,))
+       account = cursor.fetchone()
 
+       # if acount exists show error and validation checks------------------------------------------------------------------------------------------------
+       if account:
+            msg = 'Account already exists!'
+       elif not re.match(r'^[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+       elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+       elif not username or not firstname or not lastname or not password or not email:
+            msg = "Please fill out the form!"
+       else:
+            # Hash the password
+            hash = password + app.secret_key
+            hash = hashlib.sha1(hash.encode())
+            password = hash.hexdigest()
+
+            # THE %s PLACEHOLDER IN THE QUERY STRING HELPS PREVENT SQL 'INJECTION'
+            # Account doesn't exist, and the form data is valid, so insert the new account into the accounts table
+            cursor.execute('INSERT INTO user VALUES (%s, %s,%s,%s,%s)', (username,password,firstname,lastname,email))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+
+    elif request.method == 'POST':
+       # Form is empty... (no POST data)
+       msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
+
+
+
+# http://localhost:5000/pythinlogin/home - Homepage for registered users
+@app.route('/pythonlogin/home')
+def home():
+    # Check if the user is logged in
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
+
+
+@app.route('/profile')
+def profile():
+    return 'Profile Page'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+''' OLD CODE       
        password_bytes = password.encode('utf-8')
        hash_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
 
@@ -42,8 +148,7 @@ def register():
 
        session['name'] = request.form['name']
        session['email'] = request.form['email']
-       return redirect(url_for('/login')) 
-    
+       return redirect('/home') 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
@@ -75,6 +180,4 @@ def login():
 def logout():
     session.clear()
     return render_template("login.html")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+'''
