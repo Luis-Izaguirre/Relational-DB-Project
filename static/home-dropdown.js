7b99b254229eea70ccfd663abc9c2f1d420c55dd
Luis Munoz <lmunozi181i@gmail.com>
2023-11-05 13:22:45 -0800

    Phase 1 Almost complete, need to fix alert messages... Phase 2 Almost complete, need to implement a drop down menu after event handler

---
+++ static/home-dropdown.js
@@ -0,0 +1,14 @@
+
+// Add even listeners to each table row
+const tableRows = document.querySelectorAll('.table-row');
+tableRows.forEach((row,index) => {
+    row.addEventListener('click', () => {
+        const dropdown = document.querySelector('.rating-dropdown')
+        const dropdown = document.querySelector('rating')
+        const dropdown = document.querySelector('descript')
+        const dropdown = document.querySelector('save-button')
+
+        //Populate the dropdown with data from the clicked row (e.g., item's title)
+        const
+    })
+})
