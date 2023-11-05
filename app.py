f7d2fd2892e97d943eba27d7c86fcdb726148154
RithvikKrosuri <145815771+RithvikKrosuri@users.noreply.github.com>
2023-11-05 01:01:56 -0700

    updated ui

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.secret_key = "thisissecret"
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] ='root'
-app.config['MYSQL_PASSWORD'] = 'Lambda-3000'
+
 app.config['MYSQL_DB'] = 'com440'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
